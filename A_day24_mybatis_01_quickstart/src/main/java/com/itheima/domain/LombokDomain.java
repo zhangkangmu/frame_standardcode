b399ec14802a0afd5056d966859e539d245f8a74
zhangyuhong <289590351@qq.com>
2020-02-28 00:17:53 +0800

    mybatis的第一天,入门的增删改查,还有lombox的简单使用

--- A_day24_mybatis_01_quickstart/src/main/java/com/itheima/domain/LombokDomain.java
+++ A_day24_mybatis_01_quickstart/src/main/java/com/itheima/domain/LombokDomain.java
@@ -2,19 +2,19 @@ package com.itheima.domain;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
 import java.util.Date;
 
 /**
  * Created by zhangyuhong
  * Date:2020/2/27
+ * lombok的使用方式
  */
-@Setter //生成set方法
-@Getter //生成get方法
+//@Setter //生成set方法
+//@Getter //生成get方法
 @ToString(exclude = "uid") //生成toString方法，排队掉uid字段
 @AllArgsConstructor //生成全参构造器
 @NoArgsConstructor  //生成无参构造器
