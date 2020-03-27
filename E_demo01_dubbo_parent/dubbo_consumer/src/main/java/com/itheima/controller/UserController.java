9c462a21ff54a270f16e349c994515fd22264e89
zhangyuhong <289590351@qq.com>
2020-03-27 22:44:09 +0800

    dubbo分布式架构练习

---
+++ E_demo01_dubbo_parent/dubbo_consumer/src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.itheima.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2020/3/27 14:43
+ * @Version V1.0
+ */
+@Controller
+@ResponseBody // 响应不会执行视图解析器，返回json，将User对象转换成json的数据
+@RequestMapping(value = "/user")
+public class UserController {
+
+    // @Autowired
+    @Reference // 相当于：<dubbo:reference interface="com.itheima.service.UserService" id="userService"></dubbo:reference>
+    UserService userService;
+
+    // 地址栏输入：user/findById.do?id=1
+    @RequestMapping(value = "/findById")  // 注意：不要写.do，.do在web.xml中已经指定了
+    public User findById(Integer id){
+        User user = userService.findById(id);
+        return user;
+    }
+}
