0d01f7094234ff032280021b2185e535c24ee34c
zhangyuhong <289590351@qq.com>
2020-04-12 02:21:24 +0800

    mybatis逆行工程

---
+++ A_day26_mybatis_11_MyBatisGenerator/src/test/java/GeneratorSqlmap.java
@@ -0,0 +1,37 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+//使用插件的话,这个文件可以不要
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件(需要改成自己的磁盘的路径)
+		//要改的人地方
+		File configFile = new File("H:\\javaProject\\lianxi\\MyBatisGenerator\\src\\main\\resources\\generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
