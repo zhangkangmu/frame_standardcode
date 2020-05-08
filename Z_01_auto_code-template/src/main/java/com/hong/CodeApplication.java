302d386929d2298c588dc542ce4572f42d9ded44
zhangyuhong <289590351@qq.com>
2020-05-08 18:13:45 +0800

    freemarker的代码生成器

---
+++ Z_01_auto_code-template/src/main/java/com/hong/CodeApplication.java
@@ -0,0 +1,16 @@
+package com.hong;
+
+import com.hong.code.build.TemplateBuilder;
+
+/****
+ * @Author:admin
+ * @Description:
+ * @Date 2019/6/14 23:43
+ *****/
+public class CodeApplication {
+
+    public static void main(String[] args) {
+        //调用该方法即可
+        TemplateBuilder.builder();
+    }
+}
