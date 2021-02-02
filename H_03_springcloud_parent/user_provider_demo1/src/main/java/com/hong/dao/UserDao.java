2d16e5bae60156ec1d2db4acbca16147737c0c52
zhangyuhong <289590351@qq.com>
2021-02-02 23:58:38 +0800

    rocketmq的使用

--- H_03_springcloud_parent/user_provider_demo1/src/main/java/com/hong/dao/UserDao.java
+++ H_03_springcloud_parent/user_provider_demo1/src/main/java/com/hong/dao/UserDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author zhangyuhong
  * @version 1.0
  * @description com.hong.dao
- * @date 2020-4-23
+ * @date 2020-4-23mybatis.type-aliases-packag
  */
 //JpaRepository<目标,主键的数据类型>
 public interface UserDao extends JpaRepository<User,Integer> {
